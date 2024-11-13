0ba3cb91175c78041f403bed4562518b7a64fedc
SamarthKini <samarthkini@outlook.com>
2024-11-13 21:27:46 +0530

    Day one

---
+++ data_types.js
@@ -0,0 +1,8 @@
+"use strict";// this specifies the interpreter to use only the newer versions of javascript
+//readability is IMP
+console.log(typeof "samarth")
+console.log(typeof 23)
+console.log(typeof null)
+console.log(typeof undefined)
+//console.log(typeof )
+
---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234567 //#cannot be changed
+let accountEmail = "samarthkini@outlook.com"//let is used to specify this variable
+var accountPassword = "1234567890"//avoid using var as it causes issues with block scope and functional scope
+accountCity = "Mangalore"
+
+
+// accountId = 2
+
+//console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountPassword])
